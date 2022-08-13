bf6f63fa4fc4024e5b9fda553153e382fed4233e
Jonah Brüchert <jbb@kaidan.im>
2022-08-14 01:10:35 +0200

    Share more files with the upstream flatpak packaging

--- update-generated-sources.sh
+++ update-generated-sources.sh
@@ -11,6 +11,9 @@ if [ -z ${TAG} ]; then
 	exit 1
 fi
 
-wget -O generated/corrosion-generated-sources.json https://invent.kde.org/plasma-mobile/angelfish/-/raw/${TAG}/flatpak/corrosion-generated-sources.json
-wget -O generated/generated-sources.json https://invent.kde.org/plasma-mobile/angelfish/-/raw/${TAG}/flatpak/generated-sources.json
+files="kf5feedback.json corrosion.json generated-sources.json"
+
+for file in $files; do
+	wget -O generated/${file} https://invent.kde.org/plasma-mobile/angelfish/-/raw/${TAG}/flatpak/$file
+done
 
